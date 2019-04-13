3beb78d332e1688e16abd2222a4d927c98732def
Siddhant Sinha <35433185+sid573@users.noreply.github.com>
2019-04-13 17:57:26 +0530

    old website

--- matrix.py
+++ matrix.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/dimension')
+def dimension():
+    return render_template('dimension.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
 
 if __name__ == '__main__':
     app.run()
